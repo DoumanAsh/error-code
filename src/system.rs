66d211e973a3b0c373f01d5112af6a71657422d1
DoumanAsh <douman@gmx.se>
2023-09-29 09:11:37 +0900

    Remove libc for integer types by copy-pasting it from rust sources

--- src/system.rs
+++ src/system.rs
@@ -36,21 +36,21 @@ fn message(code: c_int, out: &mut crate::MessageBuf) -> &str {
     use crate::MESSAGE_BUF_SIZE;
     use core::{slice, mem};
 
-    const CP_UTF8: u32 = 65001;
-    const FORMAT_MESSAGE_FROM_SYSTEM: u32 = 0x00001000;
-    const FORMAT_MESSAGE_IGNORE_INSERTS: u32 = 0x00000200;
-    const FMT_FLAGS: u32 = FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_SYSTEM;
+    const CP_UTF8: crate::types::c_ulong = 65001;
+    const FORMAT_MESSAGE_FROM_SYSTEM: crate::types::c_ulong = 0x00001000;
+    const FORMAT_MESSAGE_IGNORE_INSERTS: crate::types::c_ulong = 0x00000200;
+    const FMT_FLAGS: crate::types::c_ulong = FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_SYSTEM;
 
     let mut buff = [mem::MaybeUninit::<u16>::uninit(); MESSAGE_BUF_SIZE * 2];
-    let mut len: u32 = unsafe {
-        FormatMessageW(FMT_FLAGS, ptr::null(), code as u32, 0, buff.as_mut_ptr() as *mut u16, buff.len() as _, ptr::null_mut())
+    let mut len = unsafe {
+        FormatMessageW(FMT_FLAGS, ptr::null(), code as crate::types::c_ulong, 0, buff.as_mut_ptr() as *mut u16, buff.len() as _, ptr::null_mut())
     };
 
     if len == 0 {
         match get_last_error() {
             //Buffer doesn't have enough space
             //But it is completely written so we'll take what we can
-            122 => len = buff.len() as u32,
+            122 => len = buff.len() as crate::types::c_ulong,
             //System cannot find specified error code
             317 => return crate::posix::message(code, out),
             _ => return crate::utils::write_message_buf(out, crate::FAIL_ERROR_FORMAT),
@@ -98,7 +98,7 @@ fn is_would_block(code: c_int) -> bool {
 
 #[cfg(windows)]
 extern "system" {
-    fn GetLastError() -> u32;
-    fn FormatMessageW(dwFlags: u32, lpSource: *const u8, dwMessageId: u32, dwLanguageId: u32, lpBuffer: *mut u16, nSize: u32, Arguments: *mut i8) -> u32;
+    fn GetLastError() -> crate::types::c_ulong;
+    fn FormatMessageW(dwFlags: crate::types::c_ulong, lpSource: *const u8, dwMessageId: crate::types::c_ulong, dwLanguageId: crate::types::c_ulong, lpBuffer: *mut u16, nSize: crate::types::c_ulong, Arguments: *mut i8) -> u32;
     fn WideCharToMultiByte(page: crate::types::c_uint, flags: crate::types::c_ulong, wide_str: *const u16, wide_str_len: c_int, multi_str: *mut i8, multi_str_len: c_int, default_char: *const i8, used_default_char: *mut bool) -> c_int;
 }
--- src/types.rs
+++ src/types.rs
@@ -1,17 +1,27 @@
 #![allow(non_camel_case_types)]
 #![allow(dead_code)]
 
-#[cfg(not(any(windows, unix)))]
-pub type c_int = i32;
-#[cfg(any(windows, unix))]
-pub use libc::c_int;
+//https://github.com/rust-lang/rust/blob/7b4d9e155fec06583c763f176fc432dc779f1fc6/library/core/src/ffi/mod.rs#L166
+#[cfg(any(target_arch = "avr", target_arch = "msp430"))]
+mod ints {
+    pub type c_int = i16;
+    pub type c_uint = u16;
+}
 
-#[cfg(not(any(windows, unix)))]
-pub type c_uint = u32;
-#[cfg(any(windows, unix))]
-pub use libc::c_uint;
+#[cfg(not(any(target_arch = "avr", target_arch = "msp430")))]
+mod ints {
+    pub type c_int = i32;
+    pub type c_uint = u32;
+}
 
-#[cfg(not(any(windows, unix)))]
-pub type c_ulong = u32;
-#[cfg(any(windows, unix))]
-pub use libc::c_ulong;
+#[cfg(all(target_pointer_width = "64", not(windows)))]
+mod longs {
+    pub type c_ulong = u64;
+}
+#[cfg(not(all(target_pointer_width = "64", not(windows))))]
+mod longs {
+    pub type c_ulong = u32;
+}
+
+pub use ints::*;
+pub use longs::*;
